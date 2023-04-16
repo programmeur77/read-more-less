db500f938a7f22f7567a0cc5064f1940258b9cc4
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-04-16 23:52:18 +0200

    Implemented the view more feature with JS

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
+const card = document.querySelector('.card-holder__content');
+const text = document.querySelector('.card__read-more-content');
+const button = document.querySelector('.card__read-btn');
 
+card.addEventListener('click', (event) => {
+  const current = event.target;
+  const isButton = current.className.includes('card__read-btn');
+
+  if (!isButton) {
+    return 0;
+  }
+
+  text.className = '.card__read-more-content--open';
+
+  button.textContent = 'Read more' ? 'Read Less' : 'Read more';
+});
