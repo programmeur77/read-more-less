09687f156ccedcc5c56c2df9810ce3030a45fffc
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-04-20 11:28:39 +0200

    Refactored code + generated text and button dynamicly and added toggle instead of className to hidden text element

--- script/script.js
+++ script/script.js
@@ -1,16 +1,33 @@
-const card = document.querySelector('.card-holder__content');
-const text = document.querySelector('.card__read-more-content');
-const button = document.querySelector('.card__read-btn');
+const allCards = document.querySelectorAll('.card-holder__content');
+ 
+allCards.forEach(card => {
+  const readMoreBtn = document.createElement('p');
+  readMoreBtn.className = 'card__read-btn';
+  readMoreBtn.innerText = 'Read More';
 
-card.addEventListener('click', (event) => {
-  const current = event.target;
-  const isButton = current.className.includes('card__read-btn');
+  const displayedText = document.createElement('p');
+  displayedText.classList.add('card__text-content');
+  displayedText.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quibusdam dolor quisquam iusto a, quidem quae laboriosam nisi itaquevoluptate libero ipsam maiores ipsum vel id doloribus incidunt ametlaudantium.';
 
-  if (!isButton) {
-    return 0;
-  }
+  const hiddenText = document.createElement('span');
+  hiddenText.classList.add('card__read-more-content');
+  hiddenText.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure commodi magnam vel corrupti fuga quam, iste sit modi doloremque nemo nam officia perspiciatis in distinctio delectus tempore. Distinctio, expedita repellendus?';
 
-  text.className = '.card__read-more-content--open';
-
-  button.textContent = 'Read more' ? 'Read Less' : 'Read more';
+  card.appendChild(displayedText);
+  displayedText.appendChild(hiddenText);
+  card.appendChild(readMoreBtn);
 });
+
+addListenerOnReadMoreBtn();
+
+function addListenerOnReadMoreBtn() {
+  const readBtn = document.querySelectorAll('.card__read-btn');
+  const hiddenText = document.querySelector('.card__read-more-content')
+  readBtn.forEach(button => {
+    button.addEventListener('click', event => {
+      event.stopPropagation();
+      hiddenText.classList.toggle('card__read-more-content--open');
+      button.textContent = 'Read more' ? 'Read less' : 'Read More';
+    });
+  });
+}
